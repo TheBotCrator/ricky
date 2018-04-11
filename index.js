d9ab938b3592f4ef62a3834d863d01c3a6abe9de
rag4214 <rag4214@rit.edu>
2018-04-11 13:09:53 -0400

    Major fix, had wrong variable being used with map

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ function convertCensorToRegex() {
     let regex = [];
 
     censor.forEach(word => {
-        let fix = word.split('').map(letter => {
+        word = word.split('').map(letter => {
             return replace.hasOwnProperty(letter) ? replace[letter] : letter;
         });
 
