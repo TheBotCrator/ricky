4547f41661c66af360d383be0ed3b0bbef98901b
Ryan Gillie <rag4214@rit.edu>
2018-07-02 16:04:29 -0400

    List of plugins in order they are evaluated

---
+++ src/plugin.config.ts
@@ -0,0 +1,7 @@
+export const PluginConfig = [
+    './plugins/filter.plugin',
+    './plugins/roles.plugin',
+    './plugins/mute.plugin',
+    './plugins/offenders.plugin',
+    './plugins/conch.plugin',
+];
