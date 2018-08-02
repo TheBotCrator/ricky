a6ae41ffd7e43203b0e6f6508229f00ecf41cecf
Ryan Gillie <rag4214@rit.edu>
2018-08-02 10:18:49 -0400

    Broke up the main logic into functions

--- src/plugins/offenders.ts
+++ src/plugins/offenders.ts
@@ -12,51 +12,18 @@ export default class Offenders extends BasePlugin {
                 // User objects of all mentioned users
                 const mentionedUsers: Discord.User[] = message.mentions.users.array();
 
-                message.delete();
-
                 // If there are mentioned users it will pull up all their info from the JSON and send the requester a message
                 // else, will send the requester the number of offenders and all of their @'s
                 if (mentionedUsers.length) {
+                    // Don't let the user know they were pinged
+                    message.delete();
+
                     mentionedUsers.forEach(user => {
-                        if (this.offenders.hasOwnProperty(user.toString())) {
-                            const userData: { [key: string]: any } = this.offenders[user.toString()];
-
-                            let sen: string = '**USER:** ' + user + '\n\n**TOTAL OFFENSES:** ' + userData['offenses'] + '\n\n**MESSAGES:**\n';
-
-                            (userData['messages'] as string[]).forEach(message => {
-                                sen += (message + '\n');
-                            });
-
-                            message.author.send(sen);
-
-                            console.log(`Sent ${message.author.tag} an offender summary of ${user.tag}`);
-                        }
-                        else {
-                            message.author.send(`${user} has 0 offenses`);
-                        }
+                        this.oneLookUp(message, user);
                     });
                 }
                 else {
-                    let count = 0;
-                    let users = '';
-
-                    for (let user in this.offenders) {
-                        if (this.offenders.hasOwnProperty(user)) {
-                            count++;
-                            users += (user + '\n');
-                        }
-                    }
-
-                    let sen = '**NUMBER OF OFFENDERS: **' + count + '\n\n';
-
-                    if (count !== 0) {
-                        sen += ('**OFFENDERS:**\n' + users);
-                    }
-
-                    message.author.send(sen);
-                    message.channel.send(`${message.member.user}, I have sent you a list of language offenders.`);
-
-                    console.log(`Sent ${message.author.tag} a list of language offenders`);
+                    this.allLookUp(message);
                 }
             }
             else {
@@ -68,5 +35,47 @@ export default class Offenders extends BasePlugin {
         else {
             return false;
         }
+    }
+
+    private allLookUp(message: Discord.Message) {
+        let count: number = 0;
+        let users: string = '';
+
+        for (let user in this.offenders) {
+            if (this.offenders.hasOwnProperty(user)) {
+                count++;
+                users += (user + '\n');
+            }
+        }
+
+        let sen = '**NUMBER OF OFFENDERS: **' + count + '\n\n';
+
+        if (count !== 0) {
+            sen += ('**OFFENDERS:**\n' + users);
+        }
+
+        message.author.send(sen);
+        message.channel.send(`${message.member.user}, I have sent you a list of language offenders.`);
+
+        console.log(`Sent ${message.author.tag} a list of language offenders`);
+    }
+
+    private oneLookUp(message: Discord.Message, user: Discord.User) {
+        if (this.offenders.hasOwnProperty(user.toString())) {
+            const userData: { [key: string]: any } = this.offenders[user.toString()];
+
+            let sen: string = '**USER:** ' + user + '\n\n**TOTAL OFFENSES:** ' + userData['offenses'] + '\n\n**MESSAGES:**\n';
+
+            (userData['messages'] as string[]).forEach(message => {
+                sen += (message + '\n');
+            });
+
+            message.author.send(sen);
+        }
+        else {
+            message.author.send(`${user} has 0 offenses`);
+        }
+
+        console.log(`Sent ${message.author.tag} an offender summary of ${user.tag}`);
     }
 }
